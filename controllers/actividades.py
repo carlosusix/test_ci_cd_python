44d99e3805f8cd52e200adea9d0526b72e577450
carlosusix <36808028+carlosusix@users.noreply.github.com>
2021-09-17 20:42:36 -0500

    chore: agregue configuraciones de github

--- controllers/actividades.py
+++ controllers/actividades.py
@@ -1,5 +1,14 @@
+from typing_extensions import Required
 from flask_restful import Resource, reqparse
 
+serializador =reqparse.RequestParser(bundle_errors=True)
+serializador.add_argument(
+    'actividadNombre',
+    required=True,
+    location='json',
+    message='Falta la actividadNombre',
+    type=str
+)
 class ActividadesController(Resource):
     def get(self):
         return {
@@ -17,3 +26,10 @@ class ActividadesController(Resource):
                 "actividadNombre":"Ir al cumpleaños de mi abuela",
             }]
         }, 201
+
+    def post(self):
+        # logica para registrar la actividad en la bd
+        return {
+            "message": "actividad creada exitosamente",
+            "content": ""
+        }
